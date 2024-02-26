b8c66b5cac9f36cbc2b4ff22cdc805dd3ab125d4
yasin-komur <ysnkomur@gmail.com>
2024-02-26 21:53:52 +0300

    Some more comments has been added to InsertionSort.java to make it clearer.

--- SortingAlgorithms/InsertionSort.java
+++ SortingAlgorithms/InsertionSort.java
@@ -11,15 +11,21 @@ public class InsertionSort
 					// addition left side of the list will be sorted this way, till the last element, then the
 					// all list is sorted.
 		{
-			int j = i - 1;
-			int x = A[i];
+			int j = i - 1; // Helper value to check values on the left. When a new value will be added, this 
+				       // j will go through the left side of the list and compare the values with x.
+
+			int x = A[i]; // This value is important the store. Because when we shift the values. we also occupy x's
+				      // place. After shifting to add the new value at the beginning of the list as the min value
+				      // in the sorted list, we have to store it to use afterwards. 
+				      // Shifted value will be on its place.
 
 			while (j > -1 && A[j] > x)
 			{
 				A[j+1] = A[j];
 				j--;
 			}
-			A[j + 1] = x;
+			A[j + 1] = x; // Add the minimum to the beginning of the array. We said j+1 because, j at the end will be -1.
+				      // And we want to add the value to index 0.
 		}
 	}
 
