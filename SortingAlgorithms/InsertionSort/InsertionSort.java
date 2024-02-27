199620fcd57ad9be2f6bb68da15fc4a94d8fc57d
yasin-komur <ysnkomur@gmail.com>
2024-02-27 21:53:50 +0300

    For practise, I deleted the file and created it again by writing the code from scratch.

--- SortingAlgorithms/InsertionSort/InsertionSort.java
+++ SortingAlgorithms/InsertionSort/InsertionSort.java
@@ -4,42 +4,44 @@ public class InsertionSort
 	{
 
 		int n = A.length;
-
-		for (int i=1; i<n; i++) // The reason we started with i=1 is important to understand.
-					// In insertion sort, we always assume that the first element in the array is already sorted.
-					// So, it's like a sorted list and we are adding next elements to that list. After every
-					// addition left side of the list will be sorted this way, till the last element, then the
-					// all list is sorted.
+		
+		for (int i=1; i<n; i++)
 		{
-			int j = i - 1; // Helper value to check values on the left. When a new value will be added, this 
-				       // j will go through the left side of the list and compare the values with x.
+			int x = A[i];
 
-			int x = A[i]; // This value is important the store. Because when we shift the values. we also occupy x's
-				      // place. After shifting to add the new value at the beginning of the list as the min value
-				      // in the sorted list, we have to store it to use afterwards. 
-				      // Shifted value will be on its place.
+			int j = i - 1;
 
 			while (j > -1 && A[j] > x)
 			{
-				A[j+1] = A[j];
+				Swap(A, j, j+1);
 				j--;
 			}
-			A[j + 1] = x; // Add the minimum to the beginning of the array. We said j+1 because, j at the end will be -1.
-				      // And we want to add the value to index 0.
+
+			A[j+1] = x;
 		}
 	}
 
+	void Swap(int A[], int i, int j)
+	{
+		int temp = A[i];
+		A[i] = A[j];
+		A[j] = temp;
+	}
+
+
 	public static void main(String[] args)
 	{
-		int arr[] = {3, 4, 1, 0, -2, 5, 11, 321, 4, 9, 9};
+		int arr[] = {6, 3, 1, 9, 10, -4};
 
-		InsertionSort insSort = new InsertionSort();
+		InsertionSort theSort = new InsertionSort();
 
-		insSort.insertionSort(arr);
+		theSort.insertionSort(arr);
 
-		for (int i=0; i<arr.length; i++)
+		for (int i=0; i< arr.length; i++)
 		{
 			System.out.print(arr[i] + " ");
 		}
 	}
 }
+
+
